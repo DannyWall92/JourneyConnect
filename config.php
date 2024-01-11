bd6fbe1d2207c8cbd979214a2175c5f61a273f33
DannyWall92 <danywall92@gmail.com>
2024-01-11 13:07:21 -0700

    Removal of API keys and db usernames and passwords.

--- config.php
+++ config.php
@@ -1,14 +1,9 @@
 <?php
 
 $host = "localhost";
-$dbuser = "dew_connect";
-$dbpass = "1!Iktomi!1";
-$dbname = "dew_connect";
+$dbuser = "db_user_name";
+$dbpass = "db_password";
+$dbname = "db_name";
 
-$stripe_pk = "pk_live_51KBum5Gfnm4c7eVfpgGB1Q9zdifkti7AsTQVprkGakxFTQTfsfyo4YpIF2Owcek81uChpMI1srbn68vANRbW5tRb00o6LMyzCn";
-$stripe_sk = "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";
-
-$pk_test = "pk_test_51KBum5Gfnm4c7eVfdtZGScO4dxPmUdgennwvyUpHWU8AwuNylWhZvaCoSSh7xiynCDz3QkW9m85BhjlSxwNKZrxW00dzKrEXOb";
-$sk_test = "sk_test_51KBum5Gfnm4c7eVfEY9vt7H19cmAdQcX8IOqPs0sDCl4KiSBrEWrjwB2uzNYRCSWStnNvAPEx7RsCnl3L6TmGoqN00A5TLFQQt";
 
 ?>
