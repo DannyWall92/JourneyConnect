bbcb25a90902b83801b5254da298c0db3d6cb288
DannyWall92 <danywall92@gmail.com>
2024-01-11 12:13:24 -0700

    Upload of correct code base

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
-$host = "localhost"; // database server connection info
-$dbuser = "db_user_name";
-$dbpass = "db_password";
-$dbname = "db_name"; // must include database name
+$host = "localhost";
+$dbuser = "dew_connect";
+$dbpass = "1!Iktomi!1";
+$dbname = "dew_connect";
 
-$stripe_pk = "STRIPE PRIVATE KEY GOES HERE";
-$stripe_sk = "STRIPE SECRET KEY GOES HERE";
+$stripe_pk = "pk_live_51KBum5Gfnm4c7eVfpgGB1Q9zdifkti7AsTQVprkGakxFTQTfsfyo4YpIF2Owcek81uChpMI1srbn68vANRbW5tRb00o6LMyzCn";
+$stripe_sk = "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";
 
-$pk_test = "STRIPE TEST PRIVATE KEY";
-$sk_test = "STRIPE TEST SECRET KEY";
+$pk_test = "pk_test_51KBum5Gfnm4c7eVfdtZGScO4dxPmUdgennwvyUpHWU8AwuNylWhZvaCoSSh7xiynCDz3QkW9m85BhjlSxwNKZrxW00dzKrEXOb";
+$sk_test = "sk_test_51KBum5Gfnm4c7eVfEY9vt7H19cmAdQcX8IOqPs0sDCl4KiSBrEWrjwB2uzNYRCSWStnNvAPEx7RsCnl3L6TmGoqN00A5TLFQQt";
 
 ?>
